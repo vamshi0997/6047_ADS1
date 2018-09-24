e26bbf55b4e5cd7c46cd6df8e13f5958f33464f1
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-09-24 13:49:36 +0530

    swap

---
+++ Swap.java
@@ -0,0 +1,11 @@
+class Swap {
+
+    public static void main(String[] args) {
+    	int a = 2, b = 3, temp;
+        temp = a;
+        a = b;
+        b = temp;
+	    System.out.println(a);
+	    System.out.println(b);
+}
+}
