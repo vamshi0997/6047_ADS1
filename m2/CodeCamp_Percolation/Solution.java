4b78c49bee13ea53bbebdd64d12b2d3f2029941c
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-09-25 21:55:50 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- m2/CodeCamp_Percolation/Solution.java
+++ m2/CodeCamp_Percolation/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 // public class Percolation {
 //     public Percolation(int n)
 // create n-by-n grid, with all sites blocked
-//     public    void open(int row, int col) 
+//     public    void open(int row, int col)
 // open site (row, col) if it is not open already
 //     public boolean isOpen(int row, int col)
 // is site (row, col) open?
@@ -20,38 +20,38 @@ import java.util.Scanner;
  * Such as Open, isopen, perculation methods. 
  */
 class Percolation {
-	/**
-	 * grid array stores the boolean values.
-	 */
-	private boolean[][] grid;
-	/**
-	 * creating WeightQuickUnionUF object.
-	 */
-	private WeightedQuickUnionUF uf;
-	/** 
-	 * varaible for number of open sites.
-	 */
-	private int numberOfOpenSites = 0;
-	/**
-	 * variable to count size.
-	 */
-	private int size;
-	/**
-	 * variable grid size is useful for virtual nodes.
-	 */
-	private int gridsize;
+    /**
+     * grid array stores the boolean values.
+     */
+    private boolean[][] grid;
+    /**
+     * creating WeightQuickUnionUF object.
+     */
+    private WeightedQuickUnionUF uf;
+    /**
+     * varaible for number of open sites.
+     */
+    private int numberOfOpenSites = 0;
+    /**
+     * variable to count size.
+     */
+    private int size;
+    /**
+     * variable grid size is useful for virtual nodes.
+     */
+    private int gridsize;
     /**
      * percolation constructor for initializing the size.
-     * 
+     *
      * @param size to count size.
      */
-	Percolation(final int size) {
-        grid = new boolean[size][size];
-        uf = new WeightedQuickUnionUF(size * size + 2);
+    Percolation(final int size1) {
+        grid = new boolean[size1][size1];
+        uf = new WeightedQuickUnionUF(size1 * size1 + 2);
         numberOfOpenSites = 0;
-        this.size = size;
-        gridsize = size * size; 
-	}
+        this.size = size1;
+        gridsize = size1 * size1;
+    }
     /**
      * In open method, we use isOpen to to find
      * whether it is open and then we use union to connect two point in grid.
@@ -59,59 +59,61 @@ class Percolation {
      * @param row grid row value.
      * @param col grid column value.
      */
-	public void open(final int row, final int col) {
-		if (grid[row][col]) return;
-		grid[row][col] = true;
-		numberOfOpenSites++;
+    public void open(final int row, final int col) {
+        if (grid[row][col]) {
+            return;
+        }
+        grid[row][col] = true;
+        numberOfOpenSites++;
 
-		//top
-		if (row - 1 >= 0 && isOpen(row - 1, col)) {
-            uf.union(getIndexOf(row,col), getIndexOf(row - 1, col));
-		}
-        
+        //top
+        if (row - 1 >= 0 && isOpen(row - 1, col)) {
+            uf.union(getIndexOf(row, col), getIndexOf(row - 1, col));
+        }
+
         //left
         if (col - 1 >= 0 && isOpen(row, col - 1)) {
-            uf.union(getIndexOf(row,col), getIndexOf(row, col - 1));
+            uf.union(getIndexOf(row, col), getIndexOf(row, col - 1));
         }
 
         //right
         if (col + 1 < size && isOpen(row, col + 1)) {
-        	uf.union(getIndexOf(row,col), getIndexOf(row, col + 1));
+            uf.union(getIndexOf(row, col), getIndexOf(row, col + 1));
         }
 
         //bottom
         if (row + 1 < size && isOpen(row + 1, col)) {
-        	uf.union(getIndexOf(row,col), getIndexOf(row + 1, col));
+            uf.union(getIndexOf(row, col), getIndexOf(row + 1, col));
         }
 
         if (row == 0) {
-        	uf.union(getIndexOf(row, col), gridsize);
+            uf.union(getIndexOf(row, col), gridsize);
         }
 
         if (row == size - 1) {
-        	uf.union(getIndexOf(row, col), gridsize + 1);
+            uf.union(getIndexOf(row, col), gridsize + 1);
         }
-	}
+    }
 
     /**
-     * Isopen return true if the grid element is open else false. 
+     * Isopen return true if the grid element is open else false.
      *
      * @param row row value.
      * @param col column value.
      *
      * @return boolean value true or false.
      */
-	public boolean isOpen(final int row, final int col) {
-		return grid[row][col];
-	}
+    public boolean isOpen(final int row, final int col) {
+        return grid[row][col];
+    }
 
     /**
      * this is used to track number of opensites.
      * @return number of opensites.
      */
-	public int countOfOpenSites() {
-		return numberOfOpenSites;
-	}
+    public int countOfOpenSites() {
+        return numberOfOpenSites;
+    }
 
     /**
      * It will give value at that particular position in array.
@@ -121,41 +123,41 @@ class Percolation {
      *
      * @return the value at that particular position in array.
      */
-	public int getIndexOf(final int row, final int col) {
-		return size * (row) + col;
-	}
+    public int getIndexOf(final int row, final int col) {
+        return size * (row) + col;
+    }
 
     /**
      * percolates is used find whether we can percolate to entire grid.
      * @return true or false.
      */
-	public boolean percolates() {
+    public boolean percolates() {
         return uf.connected(gridsize, gridsize + 1);
-	}
+    }
 }
 
 /**
  * In Solution we Start the main program.
  */
 public final class Solution {
-	/**
-	 * defalut Solution constructor.
-	 */
-	private Solution() {
+    /**
+     * defalut Solution constructor.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * In main method we take the input of array size,
-	 * and row and column values.
-	 *
-	 * @param args command line arguments.
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Percolation perculate = new Percolation(scan.nextInt());
-		while (scan.hasNext()) {
-			perculate.open(scan.nextInt() - 1, scan.nextInt() - 1);
-		}
-		System.out.println(perculate.percolates());
-	}
+    }
+    /**
+     * In main method we take the input of array size,
+     * and row and column values.
+     *
+     * @param args command line arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Percolation perculate = new Percolation(scan.nextInt());
+        while (scan.hasNext()) {
+            perculate.open(scan.nextInt() - 1, scan.nextInt() - 1);
+        }
+        System.out.println(perculate.percolates());
+    }
 }
