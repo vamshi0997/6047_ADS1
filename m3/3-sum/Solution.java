38d894c750590b80b056b8074cf1e6eb58b6cdd2
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-09-26 17:26:15 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- m3/3-sum/Solution.java
+++ m3/3-sum/Solution.java
@@ -6,10 +6,10 @@ public final class Solution {
 		int size = scan.nextInt();
 		int[] arr = new int[size];
 		int count = 0;
-		for (int i = 0; i < size; i++) {
-			for (int j = i+1; j < size; j++) {
+		for (int i = 0; i < size-2; i++) {
+			for (int j = i+1; j < size-1; j++) {
 				for (int k = j+1 ; k < size; k++) {
-                    if (arr[i] + arr[j] + arr[k] == 0) {
+                    if ((arr[i] + arr[j] + arr[k]) == 0) {
                     	count += 1;
                     }
 				}
