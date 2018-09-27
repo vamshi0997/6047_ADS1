44053b23b3458c3a645d227bd5d2edad1ced1a28
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-09-27 17:32:05 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/4.1-Balanced-Paranthesis/Solution.java
+++ m4/4.1-Balanced-Paranthesis/Solution.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+/**
+ * LinkedStack contain pop and push methods.
+ */
 class LinkedStack {
+	/**
+	 * Node first always points to top node in list.
+	 */
 	private Node first = null;
+	/**
+	 * class node creates a new node each time.
+	 */
 	class Node {
         String item;
         Node next;
@@ -10,42 +19,67 @@ class LinkedStack {
         	return this.item;
         }
 	}
-
+    /**
+     * check whether stack is empty or not.
+     * @return true or false.
+     */
 	public boolean isEmpty() {
 		return first == null;
 	}
-
+    /**
+     * push method is used to push the item.
+     * @param item element to push.
+     */
 	public void push(String item) {
 		Node oldfirst = first;
 		first = new Node();
 		first.item = item;
 		first.next = oldfirst;
 	}
-
+    /**
+     * pop method is to remove the item.
+     * @return the poped element.
+     */
 	public String pop() {
 		String item = first.item;
 		first = first.next;
 		return item;
 	}
-
+    /**
+     * Gives the top node. 
+     * @return first or top Node.
+     */
 	public Node getFirst() {
         return first;
 	}
-
-
 }
-
+/**
+ * Solution class contain main method and other methods.
+ * 
+ */
 public final class Solution {
-	public static void main (String[] args) {
+	/**
+	 * default Solution constructor.
+	 */
+    private Solution() {
+
+    }
+	/**
+	 * main method takes the inputs and send it to linkedstack class.
+	 *
+	 * @param args command args.
+	 */
+	public static void main (final String[] args) {
         Scanner scan = new Scanner(System.in);
         int num = Integer.parseInt(scan.nextLine());
-        while(scan.hasNext()) {
+        while (scan.hasNext()) {
         	int flag = 0;
         	LinkedStack link = new LinkedStack();
         	String[] str = scan.nextLine().split("");
         	//System.out.println(Arrays.toString(str));
         	for (int i = 0; i < str.length; i++) {
-                if (str[i].equals("[") || str[i].equals("{") || str[i].equals("(")) {
+                if (str[i].equals("[") || str[i].equals(
+                	"{") || str[i].equals("(")) {
                 	link.push(str[i]);
                 } else if (str[i].equals("]")) {
                 	if (link.isEmpty()) {
