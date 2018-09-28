5f82d219e8d05a13bcc6e54eba1c29b16eb14613
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-09-28 09:52:04 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ ADS-1-practice/m4/4.2-Evaluate-Postfix/Solution.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+//import java.util.Arrays;
+
+/**
+ * LinkedStack contain pop and push methods.
+ */
+class LinkedStack <E> {
+    /**
+     * Node first always points to top node in list.
+     */
+    private Node<E> first = null;
+    /**
+     * class node creates a new node each time.
+     */
+    class Node<E> {
+        /**
+         * String item name.
+         */
+        private E item;
+        /**
+         * Node next.
+         */
+        private Node next;
+        /**
+         * returns the node name.
+         * @return node.
+         */
+        public E getName() {
+            return this.item;
+        }
+    }
+    /**
+     * check whether stack is empty or not.
+     * @return true or false.
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+    /**
+     * push method is used to push the item.
+     * @param item element to push.
+     */
+    public void push(final E item) {
+        Node<E> oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+    }
+    /**
+     * pop method is to remove the item.
+     * @return the poped element.
+     */
+    public E pop() {
+        E item = first.item;
+        first = first.next;
+        return item;
+    }
+    /**
+     * Gives the top node.
+     * @return first or top Node.
+     */
+    public Node getFirst() {
+        return first;
+    }
+}
+/**
+ * Solution class contain main method and other methods.
+ */
+ public final class Solution {
+
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+
+    /**
+     * main takes input from user and prints output to console.
+     *
+     * @param      args  The commandline arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] s = scan.nextLine().split(" ");
+        LinkedStack<Integer> link = new LinkedStack<Integer>();
+        for (int i = 0; i < s.length; i++) {
+            try {
+                link.push(Integer.parseInt(s[i]));
+            } catch (Exception e) {
+                int t1 = link.pop();
+                int t2 = link.pop();
+                if (s[i].equals("*")) {
+                    link.push(t1 * t2);
+                } else if (s[i].equals("+")) {
+                    link.push(t1 + t2);
+                }
+            }
+        }
+        System.out.println(link.pop());
+    }
+
+}
