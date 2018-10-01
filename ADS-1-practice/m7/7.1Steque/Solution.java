74cda803dab4a2e014c8387b4b9afeb59a5b05eb
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-01 10:59:10 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- ADS-1-practice/m7/7.1Steque/Solution.java
+++ ADS-1-practice/m7/7.1Steque/Solution.java
@@ -3,22 +3,25 @@ import java.util.Scanner;
  * Steque class.
  */
 class Steque {
-	LinkedList<String> list1 = new LinkedList<String>();
+    /**
+     * private LinkedList.
+     */
+    private LinkedList<String> list1 = new LinkedList<String>();
     /**
      * push method.
      *
      * @param element given.
      */
     public void push(final String element) {
-    	list1.insertAtStart(element);
-    	list1.display1();
+        list1.insertAtStart(element);
+        list1.display1();
     }
     /**
      * pop method.
      */
     public void pop() {
-    	list1.deleteAtStart();
-    	list1.display1();
+        list1.deleteAtStart();
+        list1.display1();
     }
     /**
      * enqueue method.
@@ -26,8 +29,8 @@ class Steque {
      * @param element given.
      */
     public void enqueue(final String element) {
-    	list1.insertAtEnd(element);
-    	list1.display1();
+        list1.insertAtEnd(element);
+        list1.display1();
     }
 }
 
@@ -38,7 +41,7 @@ public final class Solution {
     /**
      * default constructor.
      */
-    public Solution() {
+    private Solution() {
 
     }
     /**
@@ -46,18 +49,18 @@ public final class Solution {
      *
      * @param args commandline arguments.
      */
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int testcase = Integer.parseInt(scan.nextLine());
         for (int i = 0; i < testcase; i++) {
-        	Steque sque = new Steque();
-        	while (scan.hasNext()) {
-        		String line = scan.nextLine();
-        		if (line.equals("")) {
-        			break;
-        		}
-        		String[] token = line.split(" ");
-        		switch (token[0]) {
+            Steque sque = new Steque();
+            while (scan.hasNext()) {
+                String line = scan.nextLine();
+                if (line.equals("")) {
+                    break;
+                }
+                String[] token = line.split(" ");
+                switch (token[0]) {
                     case "push":
                     sque.push(token[1]);
                     break;
@@ -69,9 +72,9 @@ public final class Solution {
                     break;
                     default:
                     break;
-        		}
-        	}
+                }
+            }
             System.out.println();
         }
-	}
+    }
 }
