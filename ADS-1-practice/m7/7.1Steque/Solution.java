b2ffb8a3957211d1cd50fefc48c1b9d5ff4ed296
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-01 10:57:17 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1-practice/m7/7.1Steque/Solution.java
+++ ADS-1-practice/m7/7.1Steque/Solution.java
@@ -1,27 +1,57 @@
 import java.util.Scanner;
+/**
+ * Steque class.
+ */
 class Steque {
 	LinkedList<String> list1 = new LinkedList<String>();
-    public void push(String element) {
+    /**
+     * push method.
+     *
+     * @param element given.
+     */
+    public void push(final String element) {
     	list1.insertAtStart(element);
     	list1.display1();
     }
+    /**
+     * pop method.
+     */
     public void pop() {
     	list1.deleteAtStart();
     	list1.display1();
     }
-    public void enqueue(String element) {
+    /**
+     * enqueue method.
+     *
+     * @param element given.
+     */
+    public void enqueue(final String element) {
     	list1.insertAtEnd(element);
     	list1.display1();
     }
 }
 
-class Solution {
-	public static void main(String[] args) {
+/**
+ * main Solution class.
+ */
+public final class Solution {
+    /**
+     * default constructor.
+     */
+    public Solution() {
+
+    }
+    /**
+     * main method.
+     *
+     * @param args commandline arguments.
+     */
+	public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int testcase = Integer.parseInt(scan.nextLine());
         for (int i = 0; i < testcase; i++) {
         	Steque sque = new Steque();
-        	while(scan.hasNext()) {
+        	while (scan.hasNext()) {
         		String line = scan.nextLine();
         		if (line.equals("")) {
         			break;
