ea875b97a40c28c465576895edd01bffbe6bf121
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-03 02:08:17 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS-1-practice/m8/8.1Team Ranking With Insertion/Solution.java
+++ ADS-1-practice/m8/8.1Team Ranking With Insertion/Solution.java
@@ -3,28 +3,29 @@ import java.util.Scanner;
  * Solution class contains main method.
  */
 public final class Solution {
-	/**
-	 * default private constructor to not provide access to others.
-	 */
-	private Solution() {
+    /**
+     * default private constructor to not provide access to others.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method is used to handle input and make use of other classes.
-	 *
-	 * @param args commandline arguments.
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Teams t1 = new Teams();
-		Teams t2 = new Teams();
-		SeletionSort s1 = new SeletionSort();
-		while(scan.hasNext()) {
-			String[] line = scan.nextLine().split(",");
-			Team t = new Team(line[0], Integer.parseInt(line[1]),
-				Integer.parseInt(line[2]), Integer.parseInt(line[3]));
-			t1.add(t);
-		}
-		System.out.println(t1.sort());
-	}
+    }
+    /**
+     * main method is used to handle input and make use of other classes.
+     *
+     * @param args commandline arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Teams t1 = new Teams();
+        Teams t2 = new Teams();
+        SeletionSort s1 = new SeletionSort();
+        while (scan.hasNext()) {
+            String[] line = scan.nextLine().split(",");
+            Team t = new Team(line[0], Integer.parseInt(line[1]),
+                Integer.parseInt(line[2]),
+                Integer.parseInt(line[2+1]));
+            t1.add(t);
+        }
+        System.out.println(t1.sort());
+    }
 }
