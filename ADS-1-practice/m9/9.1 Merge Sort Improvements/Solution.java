131a2a9a16f7a98ecd3947702db27c9e408db6ae
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-03 23:49:22 +0530

    "check_style_score -> 0 of 4 passed. style score: 1/1 "

--- ADS-1-practice/m9/9.1 Merge Sort Improvements/Solution.java
+++ ADS-1-practice/m9/9.1 Merge Sort Improvements/Solution.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
  * Solution class contain main method.
  */
 public final class Solution {
-	/**
-	 * default private constructor.
-	 */
-	private Solution() {
+    /**
+     * default private constructor.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method is used to hand input strings.
-	 */
-	public static void main(final String[] args) {
-		MergeSort m = new MergeSort();
+    }
+    /**
+     * main method is used to hand input strings.
+     * @param args command line arguments.
+     */
+    public static void main(final String[] args) {
+        MergeSort m = new MergeSort();
         Scanner scan = new Scanner(System.in);
-        while(scan.hasNext()) {
+        while (scan.hasNext()) {
             String[] line = scan.nextLine().split(",");
             m.sort(line);
             System.out.println(m.show(line));
             System.out.println();
         }
-	}
+    }
 }
