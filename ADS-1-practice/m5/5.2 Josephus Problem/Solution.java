1cafeb49664ee6f59bfe44337d221f3ba44e371a
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-06 10:01:06 +0530

    "ads1_m5_a2: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-practice/m5/5.2 Josephus Problem/Solution.java
+++ ADS-1-practice/m5/5.2 Josephus Problem/Solution.java
@@ -1,19 +1,46 @@
 import java.util.Scanner;
+/**
+ * NQueue class implements queue funtions.
+ * It contain enqueue and dequeue methods.
+ */
 class NQueue {
+    /**
+     * head Node to point first element.
+     */
     private Node head = null;
+    /**
+     * tail Node to point last element.
+     */
     private Node tail = null;
+    /**
+     * current Node to point current element.
+     */
     private Node current = null;
-
+    /**
+     * It contains data and node of next element.
+     */
     class Node {
-        int data;
-        Node next;
+        /**
+         * data variable to Store data.
+         */
+        private int data;
+        /**
+         * next is Node vatiable has next Node.
+         */
+        private Node next;
     }
-
+    /**
+     * checks whether queue is empty or Not.
+     * @return true are flase.
+     */
     public boolean isEmpty() {
         return head == null;
     }
-
-    public void enqueue(int element) {
+    /**
+     * enqueue method to enter into queue.
+     * @param element which is to be entered in the queue.
+     */
+    public void enqueue(final int element) {
         if (head == null) {
             head = new Node();
             tail = new Node();
@@ -27,10 +54,13 @@ class NQueue {
         tail = current;
     }
 
-
+    /**
+     * dequeue method to delete.
+     * @return the element that is deleted.
+     */
     public int dequeue() {
         Node temp = new Node();
-        if(head == tail) {
+        if (head == tail) {
             temp = head;
             head = null;
             tail = null;
@@ -41,48 +71,48 @@ class NQueue {
          return temp.data;
     }
 
-    // public int getNext() {
-    //     if (current == null) {
-    //         current = head;
+    // void display() {
+    //     if(head.next == null) {
+    //         System.out.println(head.data);
+    //         System.out.println("hi");
+    //         return;
     //     }
-    //     Node tmp = current;
-    //     current = tmp.next;
-    //     return tmp.data;
-    // }
-    void display() {
-        if(head.next == null) {
-            System.out.println(head.data);
-            System.out.println("hi");
-            return;
-        }
-        Node temp = new Node();
-        temp = head;
-        while(temp.next != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-        System.out.println(temp.data);
-        return;
+    //     Node temp = new Node();
+    //     temp = head;
+    //     while(temp.next != null) {
+    //         System.out.print(temp.data + " ");
+    //         temp = temp.next;
+    //     }
+    //     System.out.println(temp.data);
+    //     return;
 
-    }
+    // }
 }
 
+/**
+ * Solution class contain main method.
+ */
 public final class Solution {
+    /**
+     * default Solution constructor.
+     */
     private Solution() {
 
     }
-    public static void main(String[] args) {
+    /**
+     * main class to handle input and output.
+     *
+     * @param args commandline arguments.
+     */
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
         while (scan.hasNext()) {
-        	NQueue n = new NQueue();
-        	int count = scan.nextInt();
-        	int cut = scan.nextInt();
-            // n.enqueue(0);
-            // n.enqueue(1);
-            // n.enqueue(2);
+            NQueue n = new NQueue();
+            int count = scan.nextInt();
+            int cut = scan.nextInt();
 
-            for(int i = 0; i < count; i++) {
+            for (int i = 0; i < count; i++) {
                 n.enqueue(i);
             }
             //n.display();
@@ -91,22 +121,20 @@ public final class Solution {
             // }
 
 
-            String display = "";
+            String result = "";
             while (!n.isEmpty()) {
                 int t2 = 0, t1 = 0;
                 for (int i = 0; i < cut; i++) {
-                    if(i != cut - 1) {
+                    if (i != cut - 1) {
                         t1 = n.dequeue();
                         n.enqueue(t1);
-                    }
-                    else {
+                    } else {
                         t2 = n.dequeue();
                     }
                 }
-                display += t2 + " ";
+                result += t2 + " ";
             }
-            System.out.println(display);
-            
+            System.out.println(result.trim());
         }
     }
 }
