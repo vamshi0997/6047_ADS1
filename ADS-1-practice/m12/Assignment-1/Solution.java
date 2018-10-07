db71e5cced6d19e5c6164065487cfec5f8adc8c5
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-07 20:44:24 +0530

    "ads1_m12_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-practice/m12/Assignment-1/Solution.java
+++ ADS-1-practice/m12/Assignment-1/Solution.java
@@ -3,30 +3,36 @@ import java.util.Scanner;
  * Solution class contains main method.
  */
 public final class Solution {
-	/**
-	 * default constructor.
-	 */
-	private Solution() {
-
-	}
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int Qualified = Integer.parseInt(scan.nextLine());
-		int vacancies = Integer.parseInt(scan.nextLine());
-		int unreserved = Integer.parseInt(scan.nextLine());
-		int bcvacancies = Integer.parseInt(scan.nextLine());
-		int scvacancies = Integer.parseInt(scan.nextLine());
-		int stvancancies = Integer.parseInt(scan.nextLine());
-		Selection sel = new Selection();
-		while (scan.hasNext()) {
-			String[] line = scan.nextLine().split(",");
-			Student s = new Student(line[0], line[1],
-				Integer.parseInt(line[2]), Integer.parseInt(line[3]),
-				Integer.parseInt(line[4]), Integer.parseInt(line[5]), line[6]);
+    /**
+     * default constructor.
+     */
+    private Solution() {
+    }
+    /**
+     * main method is used to handle input and outputs.
+     *
+     * @param args commandline arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int qualified = Integer.parseInt(scan.nextLine());
+        int vacancies = Integer.parseInt(scan.nextLine());
+        int unreserved = Integer.parseInt(scan.nextLine());
+        int bcvacancies = Integer.parseInt(scan.nextLine());
+        int scvacancies = Integer.parseInt(scan.nextLine());
+        int stvancancies = Integer.parseInt(scan.nextLine());
+        Selection sel = new Selection();
+        while (scan.hasNext()) {
+            String[] line = scan.nextLine().split(",");
+            Student s = new Student(line[0], line[1],
+                Integer.parseInt(line[2]), Integer.parseInt(line[2 + 1]),
+                Integer.parseInt(line[2 + 2]), Integer.parseInt(
+                    line[2 + 2 + 1]), line[2 + 2 + 2]);
             sel.add(s);
-		}
-		System.out.println(sel.sort());
-		System.out.println();
-		System.out.println(sel.sort1(vacancies, unreserved, bcvacancies, scvacancies, stvancancies));
-	}
+        }
+        System.out.println(sel.sort());
+        System.out.println();
+        System.out.println(sel.sort1(
+            vacancies, unreserved, bcvacancies, scvacancies, stvancancies));
+    }
 }
--- ADS-1-practice/m12/Assignment-1/Student.java
+++ ADS-1-practice/m12/Assignment-1/Student.java
@@ -1,114 +1,141 @@
 /**
  * Student class contain Student details and other methods.
  */
-class Student implements Comparable<Student>{
-	private String studentName;
-	private String dob;
-	String[] ndob = null;
-	private int date = 0;
-	private int month = 0;
-	private int year = 0;
-	private int subject1;
-	private int subject2;
-	private int subject3;
-	private int total;
-	private String reservationcat;
+class Student implements Comparable<Student> {
+    /**
+     * student name contains name of student.
+     */
+    private String studentName;
+    /**
+     * dob contains date of birth.
+     */
+    private String dob;
+    /**
+     * it contains dob string.
+     */
+    private String[] ndob = null;
+    /**
+     * it contains date.
+     */
+    private int date = 0;
+    /**
+     * It contains month.
+     */
+    private int month = 0;
+    /**
+     * It contains year.
+     */
+    private int year = 0;
+    /**
+     * It contains subject1 marks.
+     */
+    private int subject1;
+    /**
+     * It contains subject2 marks.
+     */
+    private int subject2;
+    /**
+     * It contains subject3 marks.
+     */
+    private int subject3;
+    /**
+     * It contains subject3 marks.
+     */
+    private int total;
+    /**
+     * it contains reservation category.
+     */
+    private String reservationcat;
+    /**
+     * Student constructor.
+     *
+     * @param name name.
+     * @param dob1 dob.
+     * @param sub1 sub1
+     * @param sub2 sub2.
+     * @param sub3 sub3
+     * @param tot tot.
+     * @param res res.
+     */
+    Student(final String name, final String dob1, final int sub1,
+        final int sub2, final int sub3, final int tot, final String res) {
+        this.studentName = name;
+        this.dob = dob1;
+        this.ndob = dob1.split("-");
+        this.date = Integer.parseInt(ndob[0]);
+        this.month = Integer.parseInt(ndob[1]);
+        this.year = Integer.parseInt(ndob[2]);
+        this.subject1 = sub1;
+        this.subject2 = sub2;
+        this.subject3 = sub3;
+        this.total = tot;
+        this.reservationcat = res;
 
-	Student(final String name, final String dob1, final int sub1,
-		final int sub2, final int sub3, final int tot, final String res) {
-		this.studentName = name;
-		this.dob = dob1;
-		this.ndob = dob1.split("-");
-		this.date = Integer.parseInt(ndob[0]);
-		this.month = Integer.parseInt(ndob[1]);
-		this.year = Integer.parseInt(ndob[2]);
-		this.subject1 = sub1;
-		this.subject2 = sub2;
-		this.subject3 = sub3;
-		this.total = tot;
-		this.reservationcat = res;
-
-	}
-
-	public String getName() {
-		return this.studentName;
-	}
-
-	public int getTot() {
+    }
+    /**
+     * Name of Student.
+     * @return name.
+     */
+    public String getName() {
+        return this.studentName;
+    }
+    /**
+     * get total marks.
+     * @return int.
+     */
+    public int getTot() {
         return this.total;
-	}
-
-	public String getCat() {
-		return this.reservationcat;
-	}
-
-	public int compareTo(Student stud) {
-		if (this.total > stud.total) {
-			return 1;
-		}
-		if (this.total < stud.total) {
-			return -1;
-		}
-		if (this.subject3 > stud.subject3) {
-			return 1;
-		}
-		if (this.subject3 < stud.subject3) {
-			return -1;
-		}
-		if (this.subject2 > stud.subject2) {
-			return 1;
-		}
-		if (this.subject2 < stud.subject2) {
-			return -1;
-		}
-		if (this.year > stud.year) {
-			return 1;
-		}
-		if (this.year < stud.year) {
-			return -1;
-		}
-		if (this.month > stud.month) {
-			return 1;
-		}
-		if (this.month < stud.month) {
-			return -1;
-		}
-		if (this.date > stud.date) {
-			return 1;
-		}
-		if (this.date < stud.date) {
-			return -1;
-		}
-
-
-		return 0;
-	}
-
-	// static Comparator<Student> merit = new Comparator<Student>(){
-	// 	public int compare(Student s1, Student s2) {
-	// 		if (s1.total - s2.total != 0) {
-	// 			return s1.total - s2.total;
-	// 		} else if(s1.subject3 -s2.subject3 != 0) {
- //                return s1.subject3 -s2.subject3;
-	// 		} else if(s1.subject2 -s2.subject2 != 0) {
-	// 			return;
-	// 		}
-	// 	}
-	// };
-
-	// static Comparator<Student> reserve = new Comparator<Student>(){
-	// 	public int compare(Student s1, Student s2) {
-	// 		if (s1.total - s2.total != 0) {
-	// 			return s1.total - s2.total;
-	// 		}
-	// 	}
-	// };
-
-	public String toString() {
-        return this.studentName + ","
-        + this.total + "," + this.reservationcat;
-	}
-
-
-
+    }
+    /**
+     * get reservation category.
+     * @return category.
+     */
+    public String getCat() {
+        return this.reservationcat;
+    }
+    /**
+     * comapares the required parameters in student.
+     *
+     * @param stud Student.
+     * @return int value.
+     */
+    public int compareTo(final Student stud) {
+        if (this.total > stud.total) {
+            return 1;
+        }
+        if (this.total < stud.total) {
+            return -1;
+        }
+        if (this.subject3 > stud.subject3) {
+            return 1;
+        }
+        if (this.subject3 < stud.subject3) {
+            return -1;
+        }
+        if (this.subject2 > stud.subject2) {
+            return 1;
+        }
+        if (this.subject2 < stud.subject2) {
+            return -1;
+        }
+        if (this.year > stud.year) {
+            return 1;
+        }
+        if (this.year < stud.year) {
+            return -1;
+        }
+        if (this.month > stud.month) {
+            return 1;
+        }
+        if (this.month < stud.month) {
+            return -1;
+        }
+        if (this.date > stud.date) {
+            return 1;
+        }
+        if (this.date < stud.date) {
+            return -1;
+        }
+        return 0;
+    }
 }
