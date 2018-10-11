9739616116bae64fd129c6dc3ea3ad1c54d87952
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-11 20:23:18 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-practice/m16/16.1 BST/Solution.java
+++ ADS-1-practice/m16/16.1 BST/Solution.java
@@ -3,32 +3,34 @@ import java.util.Scanner;
  * Solution class contain main method.
  */
 public final class Solution {
-	/**
-	 * private default constructor.
-	 */
-	private Solution() {
+    /**
+     * private default constructor.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method is used to handle input and other output.
-	 *
-	 * @param args command line arguments.
-	 */
-	public static void main(String[] args) {
+    }
+    /**
+     * main method is used to handle input and other output.
+     *
+     * @param args command line arguments.
+     */
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         BST<Book, Integer> bst = new BST<Book, Integer>();
         while (scan.hasNext()) {
-        	String[] input = scan.nextLine().split(",");
-        	Book b = new Book(input[1], input[2], Float.parseFloat(input[3]));
-        	switch(input[0]) {
-        		case "put":
-                bst.put(b, Integer.parseInt(input[4]));
+            String[] input = scan.nextLine().split(",");
+            Book bk = new Book(input[1], input[2], Float.parseFloat(input[3]));
+            switch(input[0]) {
+                case "put":
+                bst.put(bk, Integer.parseInt(input[4]));
                 break;
                 case "get":
-                System.out.println( bst.get(b));
+                System.out.println( bst.get(bk));
                 break;
-        	}
+                default:
+                break;
+            }
 
         }
-	}
+    }
  }
