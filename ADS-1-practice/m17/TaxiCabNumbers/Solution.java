39d4de5e4ca3b34a0dad15e2fde283f389d74b27
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-12 17:09:14 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-practice/m17/TaxiCabNumbers/Solution.java
+++ ADS-1-practice/m17/TaxiCabNumbers/Solution.java
@@ -1,26 +1,56 @@
 import java.util.Scanner;
+/**
+ * Solution class contain main method and
+ * other methods such as compareTo etc..,
+ */
 public class Solution implements Comparable<Solution> {
+    /**
+     * sum is of min type.
+     */
     private final int sum;
-    private final int i;
-    private final int j;
-
-    public Solution(int i, int j) {
-        this.sum = i*i*i + j*j*j;
-        this.i = i;
-        this.j = j;
+    /**
+     * first is of int type.
+     */
+    private final int first;
+    /**
+     * second is of int type.
+     */
+    private final int second;
+    /**
+     * parameterized Solution constructor.
+     *
+     * @param first1 The first
+     * @param second1 The second.
+     */
+    public Solution(int first1, int second1) {
+        this.sum = first1 * first1 * first1 + second1 * second1 * second1;
+        this.first = first1;
+        this.second = second1;
     }
-
+    /**
+     * CompareTo method compares first and second sum.
+     *
+     * @param that That object.
+     * @return integer.
+     */
     public int compareTo(Solution that) {
         if (this.sum < that.sum) return -1;
         if (this.sum > that.sum) return +1;
         return 0;
     }
-
+    /**
+     * Print the value in required string format.
+     * @return String.
+     */ 
     public String toString() {
-        return sum + " = " + i + "^3" + " + " + j + "^3";
+        return sum + " = " + first + "^3" + " + " + second + "^3";
     }
 
-    public static void main(String[] args) {
+    /**
+     * Main method is used to handle the input.
+     * @param args command line arguments.
+     */
+    public static void main(final String[] args) {
         int n = 600, temp = -1, count = 0;
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
@@ -47,8 +77,8 @@ public class Solution implements Comparable<Solution> {
             }
 
             temp = s.sum;
-            if (s.j < n)
-                pq.insert(new Solution(s.i, s.j + 1));
+            if (s.second < n)
+                pq.insert(new Solution(s.first, s.second + 1));
         }
     }
 }
