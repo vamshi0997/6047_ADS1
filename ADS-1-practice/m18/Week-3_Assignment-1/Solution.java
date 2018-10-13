f4ccf16bd522c130ff52a9dfb82d076dc20c24d0
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-13 11:51:52 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-practice/m18/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+/**
+ * Solution class contain main method.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object for solution.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * main method is used to handle the inputs.
+	 *
+	 * @param      args  The command line arguments
+	 */
+	public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int nStocks = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < 6; i++) {
+        	MinPQ<Stock> min = new MinPQ<Stock>();
+        	//MaxPQ<String> max = new MaxPQ<String>();
+            for(int j = 0; j < nStocks; j++) {
+        	    String[] input = scan.nextLine().split(",");
+        	    Stock s = new Stock(input[0], Double.parseDouble(input[1]));
+        	    min.insert(s);
+        	    //max.insert(s);
+            }
+            for(int k = 0; k < 5; k++) {
+            	System.out.println(min.delMin().getName());
+            }
+            // for(int l = 0; l < 5; l++) {
+            // 	System.out.println(delMax());
+            // }
+
+        }
+
+	}
+}
---
+++ ADS-1-practice/m18/Week-3_Assignment-1/Stock.java
@@ -0,0 +1,24 @@
+class Stock implements Comparable<Stock> {
+	private String name;
+	private Double change;
+	Stock(String name1, Double change1) {
+		this.name = name1;
+		this.change = change1;
+	}
+	String getName() {
+		return this.name;
+	}
+	Double getchange() {
+		return this.change;
+	}
+	
+	public int compareTo(Stock that) {
+        if(this.change > that.change) {
+        	return -1;
+        }
+        if( this.change < that.change) {
+        	return 1;
+        }
+        return this.getName().compareTo(this.getName());
+	}
+}
