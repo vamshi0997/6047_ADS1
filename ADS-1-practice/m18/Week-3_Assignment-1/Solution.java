cab368c93009491624a728ca79d80a8a0952dc6f
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-13 13:20:10 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-practice/m18/Week-3_Assignment-1/Solution.java
+++ ADS-1-practice/m18/Week-3_Assignment-1/Solution.java
@@ -17,34 +17,50 @@ public final class Solution {
 	public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int nStocks = Integer.parseInt(scan.nextLine());
-        BinarySearchST<Stock, Integer> bsst = new BinarySearchST<Stock, Integer>();
+        String str2 = "";
+        BinarySearchST<String, Integer> bsst = new BinarySearchST<String, Integer>();
         for (int i = 0; i < 6; i++) {
         	MinPQ<Stock> min = new MinPQ<Stock>();
         	MaxPQ<Stock> max = new MaxPQ<Stock>();
-            for(int j = 0; j < nStocks; j++) {
+            for (int j = 0; j < nStocks; j++) {
         	    String[] input = scan.nextLine().split(",");
         	    Stock s = new Stock(input[0], Double.parseDouble(input[1]));
         	    min.insert(s);
         	    max.insert(s);
             }
-            for(int k = 0; k < 5; k++) {
+            for (int k = 0; k < 5; k++) {
             	String str = min.delMin().tostr();
+            	String[] str1 =str.split(" ");
+            	str2 +=  str;
+
+                //String strFind = "Java";
+                int count = 0, fromIndex = 0;
+        
+                while ((fromIndex = str2.indexOf(str1[0], fromIndex)) != -1 ){
+ 
+                 //System.out.println("Found at index: " + fromIndex);
+                 count++;
+                 fromIndex++;
+                }
+                //System.out.println(count);
+            	bsst.put(str1[0], count);
             	System.out.println(str);
             }
             System.out.println();
-            for(int l = 0; l < 5; l++) {
+            for (int l = 0; l < 5; l++) {
             	System.out.println(max.delMax().tostr());
             }
             System.out.println();
         }
         int input = Integer.parseInt(scan.nextLine());
-        for(int a = 0; a < input; a++) {
+        for (int a = 0; a < input; a++) {
         	String[] bstinp = scan.nextLine().split(",");
-        	if(bstinp[0].equals("get")) {
-
-        	}
+        	if (bstinp[0].equals("get")) {
+                if(bstinp[1].equals("minST")) {
+                	//System.out.println(bsst.get());
+                	//System.out.println(bstinp[2]);
+        	    }
+            }
         }
-
-
-	}
+    }
 }
