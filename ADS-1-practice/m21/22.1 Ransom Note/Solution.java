719b084bfb0ee0db7e53516b7b23d1945b0ccac5
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-25 16:16:55 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-practice/m21/22.1 Ransom Note/Solution.java
+++ ADS-1-practice/m21/22.1 Ransom Note/Solution.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.HashSet;
+import java.util.Hashtable;
 /**
  * Class for solution.
  */
@@ -12,25 +12,36 @@ class Solution {
     }
     public static void main(String[] args) {
     	Scanner scan = new Scanner(System.in);
-        HashSet<String> m = new HashSet<String>();
-        HashSet<String> m1 = new HashSet<String>();
-        // int a = Integer.parseInt(scan.nextLine());
-        // int b = Integer.parseInt(scan.nextLine());
+        Hashtable<String, Integer> m = new Hashtable<String, Integer>();
+        Hashtable<String, Integer> m1 = new Hashtable<String, Integer>();
         String input[] = scan.nextLine().split(" ");
         String[] magazine = scan.nextLine().split(" ");
         String[] note = scan.nextLine().split(" ");
         for(int i = 0; i < magazine.length; i++) {
-            m.add(magazine[i]);
+            Integer count = m.get(magazine[i]);
+            if (m.containsKey(magazine[i])) {
+                m.put(magazine[i], count + 1);
+            } else {
+                m.put(magazine[i], 1);
+            }
         }
+
         for(int j = 0; j < note.length; j++) {
-            if (!m.contains(note[j])) {
+            if (!m.containsKey(note[j])) {
                 System.out.println("No");
                 return;
-            } else if (m.contains(note[j]) && m1.contains(note[j])) {
-                System.out.println("No");
-                return;
-            } else if(m.contains(note[j])) {
-                m1.add(note[j]);
+            }
+            else if (m.containsKey(note[j])) {
+                Integer num = m1.get(note[j]);
+                if(m1.containsKey(note[j])) {
+                    m1.put(note[j], num + 1);
+                } else {
+                    m1.put(note[j], 1);
+                }
+                if (m1.get(note[j]) > m.get(note[j])) {
+                    System.out.println("No");
+                    return;
+                }
             }
         }
         System.out.println("Yes");
