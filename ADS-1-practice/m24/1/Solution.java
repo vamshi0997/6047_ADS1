832da654d7ad5679e7ce691740c34a3ff1933fa6
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-27 11:06:27 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-practice/m24/1/Solution.java
+++ ADS-1-practice/m24/1/Solution.java
@@ -17,24 +17,26 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int num = Integer.parseInt(scan.nextLine());
-        SeparateChainingHashST<String, Student> scht = new SeparateChainingHashST<String, Student>();
-        for(int i = 0; i < num; i++) {
+        SeparateChainingHashST<String, Student> scht =
+        new SeparateChainingHashST<String, Student>();
+        for (int i = 0; i < num; i++) {
             String[] input = scan.nextLine().split(",");
-            Student s = new Student(input[0], input[1], Double.parseDouble(input[2]));
+            Student s = new Student(
+                input[0], input[1], Double.parseDouble(input[2]));
             scht.put(input[0], s);
         }
         int num1 = Integer.parseInt(scan.nextLine());
-        for(int j = 0; j < num1; j++) {
+        for (int j = 0; j < num1; j++) {
             String[] ginput = scan.nextLine().split(" ");
             Student sd;
             try {
                 sd = scht.get(ginput[1]);
-            if(ginput[2].equals("1")) {
+            if (ginput[2].equals("1")) {
                 System.out.println(sd.getName());
             } else {
                 System.out.println(sd.getMarks());
             }
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println("Student doesn't exists...");
             }
         }
--- ADS-1-practice/m24/1/Student.java
+++ ADS-1-practice/m24/1/Student.java
@@ -1,40 +1,54 @@
 /**
  * Student class contains roll number and name and Marks of the Student.
- * 
  */
 class Student {
-	/**
-	 * rollno of the Student.
-	 */
-	private String rollno;
-	/**
-	 * Name of the Student.
-	 */
-	private String name;
-	/**
-	 * Total marks of the student.
-	 */
-	private Double marks;
-	/**
-	 * Constructs the object for Student.
-	 *
-	 * @param      roll    The roll of the Student.
-	 * @param      name1   The name of the Student.
-	 * @param      marks1  The marks of the Student.
-	 */
-	Student(final String roll, final String name1, final Double marks1) {
-		this.rollno = roll;
-		this.name = name1;
-		this.marks = marks1;
-	}
-	String getRoll() {
-		return this.rollno;
-	}
-	String getName() {
-		return this.name;
-	}
-	Double getMarks() {
-		return this.marks;
-	}
+    /**
+     * rollno of the Student.
+     */
+    private String rollno;
+    /**
+     * Name of the Student.
+     */
+    private String name;
+    /**
+     * Total marks of the student.
+     */
+    private Double marks;
+    /**
+     * Constructs the object for Student.
+     *
+     * @param      roll    The roll of the Student.
+     * @param      name1   The name of the Student.
+     * @param      marks1  The marks of the Student.
+     */
+    Student(final String roll, final String name1, final Double marks1) {
+        this.rollno = roll;
+        this.name = name1;
+        this.marks = marks1;
+    }
+    /**
+     * Gets the roll number of the Student.
+     *
+     * @return     The roll.
+     */
+    public String getRoll() {
+        return this.rollno;
+    }
+    /**
+     * Gets the name of the Student.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return this.name;
+    }
+    /**
+     * Gets the Total marks of the Student.
+     *
+     * @return     The marks.
+     */
+    public Double getMarks() {
+        return this.marks;
+    }
 
 }
