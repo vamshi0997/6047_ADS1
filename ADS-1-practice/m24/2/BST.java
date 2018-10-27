5887b14382ba30d5d8c5e89c57dbac63fff39550
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-27 13:08:46 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-practice/m24/2/BST.java
+++ ADS-1-practice/m24/2/BST.java
@@ -423,7 +423,9 @@ class BST<Key extends Comparable<Key>, Value> {
      * @return all keys in the symbol table
      */
     public Iterable<Key> keys() {
-        if (isEmpty()) return new Queue<Key>();
+        if (isEmpty()) {
+            return new Queue<Key>();
+        }
         return keys(min(), max());
     }
 
@@ -433,27 +435,51 @@ class BST<Key extends Comparable<Key>, Value> {
      *
      * @param  lo minimum endpoint
      * @param  hi maximum endpoint
-     * @return all keys in the symbol table between {@code lo} 
+     * @return all keys in the symbol table between {@code lo}
      *         (inclusive) and {@code hi} (inclusive)
      * @throws IllegalArgumentException if either {@code lo} or {@code hi}
      *         is {@code null}
      */
-    public Iterable<Key> keys(Key lo, Key hi) {
-        if (isEmpty()) return new Queue<Key>();
-        if (lo == null) throw new IllegalArgumentException("first argument to keys() is null");
-        if (hi == null) throw new IllegalArgumentException("second argument to keys() is null");
+    public Iterable<Key> keys(final Key lo, final Key hi) {
+        if (isEmpty()) {
+            return new Queue<Key>();
+        }
+        if (lo == null) {
+            throw new IllegalArgumentException(
+                "first argument to keys() is null");
+        }
+        if (hi == null) {
+            throw new IllegalArgumentException(
+                "second argument to keys() is null");
+        }
 
         Queue<Key> queue = new Queue<Key>();
         keys(root, queue, lo, hi);
         return queue;
-    } 
-
-    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) { 
-        if (x == null) return; 
-        int cmplo = lo.compareTo(x.key); 
-        int cmphi = hi.compareTo(x.key); 
-        if (cmplo < 0) keys(x.left, queue, lo, hi); 
-        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key); 
-        if (cmphi > 0) keys(x.right, queue, lo, hi); 
+    }
+    /**
+     * Iterating all the keys in array.
+     *
+     * @param      x      Node.
+     * @param      queue  The queue
+     * @param      lo     The lower
+     * @param      hi     The higher
+     */
+    private void keys(
+        final Node x, final Queue<Key> queue, final Key lo, final Key hi) {
+        if (x == null) {
+            return;
+        }
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) {
+            keys(x.left, queue, lo, hi);
+        }
+        if (cmplo <= 0 && cmphi >= 0) {
+            queue.enqueue(x.key);
+        }
+        if (cmphi > 0) {
+            keys(x.right, queue, lo, hi);
+        }
     }
 }
