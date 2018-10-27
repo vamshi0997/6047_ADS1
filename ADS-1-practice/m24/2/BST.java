99c435500121a6ca4a9fd5916cb3590bfe65f42b
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-27 12:52:44 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-practice/m24/2/BST.java
@@ -0,0 +1,459 @@
+import java.util.NoSuchElementException;
+/**
+ * Class for binary search tree.
+ * @param      <Key>     The key
+ * @param      <Value>  The values
+ */
+class BST<Key extends Comparable<Key>, Value> {
+    /**.
+     * starting element in the BST.
+     */
+    private Node root;
+    /**.
+     * Class for node.
+     */
+    class Node {
+        /**.
+         * Object of Book class with book details.
+         */
+        private Key key;
+        /**.
+         * value of the book in Book class
+         */
+        private Value value;
+        /**.
+         * Left and Right node of the BST
+         */
+        private Node left, right;
+        /**
+         * size.
+         */
+        private int size;
+        /**
+         * Constructs the object.
+         * @param      k     key.
+         * @param      v     value.
+         * @param      s     size.
+         */
+        Node(final Key k, final Value v, final int s) {
+            key = k;
+            value = v;
+            size = s;
+        }
+    }
+    /**
+     * Determines if empty.
+     * @return     True if empty, False otherwise.
+     * Time complexity for this method is O(1).
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+    /**
+     * size method that returns the size.
+     * @return     size of type int.
+     * Time complexity for this method is O(1).
+     */
+    public int size() {
+        return size(root);
+    }
+    /**
+     * size method that returns the size.
+     * @param      x     Node.
+     * @return     size of type int.
+     * Time complexity for this method is O(1).
+     */
+    private int size(final Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.size;
+        }
+    }
+    /**.
+     * This method is to add a key and value to BST.
+     * Time Complexity is O(N).
+     * because it calls another put method to add elements.
+     *
+     * @param      key    The key
+     * @param      value  The value
+     */
+    public void put(final Key key, final Value value) {
+        if (key == null) {
+            return;
+        }
+        root = put(root, key, value);
+    }
+    /**.
+     * This method is to add element to BST
+     * Time Complexity is O(N) for worst case.
+     * element is added until it reaches to the
+     * position it should added at.
+     *
+     * @param      node   The node
+     * @param      key    The key
+     * @param      value  The value
+     *
+     * @return     returns node
+     */
+    public Node put(final Node node, final Key key,
+                    final Value value) {
+        if (node == null) {
+            return new Node(key, value, 1);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+    /**.
+     * This method is to return the value of that key
+     * Time Complexity is O(N)
+     *
+     * @param      key   The key
+     *
+     * @return     returns the value.
+     */
+    public Value get(final Key key) {
+        return get(root, key);
+    }
+    /**.
+     * This method is to return the value of that key
+     * Time Complexity is O(N)
+     *
+     * @param      node  the node where the book details and
+     *                   values.
+     * @param      key   The key
+     *
+     * @return     returns the value of that key.
+     */
+    private Value get(final Node node, final Key key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return get(node.left, key);
+        } else if (cmp > 0) {
+            return get(node.right, key);
+        } else {
+            return node.value;
+        }
+    }
+    /**
+     * min method that returns the minimum node.
+     * @return     Key of type Book.
+     * Time complexity for this method is O(N).
+     */
+    public Key min() {
+        return min(root).key;
+    }
+    /**
+     * min method that returns the minimum node.
+     * @param      x     Node.
+     * @return     Node.
+     * Time complexity for this method is O(N).
+     */
+    private Node min(final Node x) {
+        if (x.left == null) {
+            return x;
+        } else {
+            return min(x.left);
+        }
+    }
+    /**
+     * max method that returns the maximum node.
+     * @return     Key of type Book.
+     * Time complexity for this method is O(N).
+     */
+    public Key max() {
+        return max(root).key;
+    }
+    /**
+     * min method that returns the minimum node.
+     * @param      x     Node.
+     * @return     Node.
+     * Time complexity for this method is O(N).
+     */
+    private Node max(final Node x) {
+        if (x.right == null) {
+            return x;
+        } else {
+            return max(x.right);
+        }
+    }
+    /**
+     * floor method that returns the node less
+     * than given node.
+     * @param      key   The key
+     * @return     Key of type Book.
+     * Time complexity for this method is O(N).
+     */
+    public Key floor(final Key key) {
+        Node x = floor(root, key);
+        if (x == null) {
+            return null;
+        } else {
+            return x.key;
+        }
+    }
+    /**
+     * floor method that returns the node less
+     * than given node.
+     * @param      x     Node.
+     * @param      key   The key
+     * @return     Node.
+     * Time complexity for this method is O(N).
+     */
+    private Node floor(final Node x, final Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            return x;
+        }
+        if (cmp < 0) {
+            return floor(x.left, key);
+        }
+        Node t = floor(x.right, key);
+        if (t != null) {
+            return t;
+        } else {
+            return x;
+        }
+    }
+    /**
+     * ceiling method that returns the node greater
+     * than given node.
+     * @param      key   The key
+     * @return     Key of type Book.
+     * Time complexity for this method is O(N).
+     */
+    public Key ceiling(final Key key) {
+        Node x = ceiling(root, key);
+        if (x == null) {
+            return null;
+        } else {
+            return x.key;
+        }
+    }
+    /**
+     * ceiling method that returns the node greater
+     * than given node.
+     * @param      x     Node.
+     * @param      key   The key
+     * @return     Node.
+     * Time complexity for this method is O(N).
+     */
+    private Node ceiling(final Node x, final Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            return x;
+        }
+        if (cmp < 0) {
+            Node t = ceiling(x.left, key);
+            if (t != null) {
+                return t;
+            } else {
+                return x;
+            }
+        }
+        return ceiling(x.right, key);
+    }
+    /**
+     * select method that returns the node at particular index.
+     * @param      k     index.
+     * @return     Key of type Book.
+     * Time complexity for this method is O(N).
+     */
+    public Key select(final int k) {
+        Node x = select(root, k);
+        return x.key;
+    }
+    /**
+     * select method that returns the node at particular index.
+     * @param      x     Node.
+     * @param      k     index.
+     * @return     Node.
+     * Time complexity for this method is O(N).
+     */
+    private Node select(final Node x, final int k) {
+        if (x == null) {
+            return null;
+        }
+        int t = size(x.left);
+        if (t > k) {
+            return select(x.left, k);
+        } else if (t < k) {
+            return select(x.right, k - t - 1);
+        } else {
+            return x;
+        }
+    }
+
+    /**
+     * Time complexity is O(N).
+     * Removes the smallest key and associated value from the symbol table.
+     *
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public void deleteMin() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Symbol table underflow");
+        }
+        root = deleteMin(root);
+        //assert check();
+    }
+    /**
+     * Time complexity is O(N).
+     * delete Min from BST.
+     *
+     * @param      x     Min Node to be deleted.
+     *
+     * @return     deleted Node.
+     */
+    private Node deleteMin(final Node x) {
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    /**
+     * Time complexity is O(N).
+     * Removes the largest key and associated value from the symbol table.
+     *
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public void deleteMax() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Symbol table underflow");
+        }
+        root = deleteMax(root);
+        //assert check();
+    }
+    /**
+     * Time complexity is O(N).
+     * delete the Maximum Node in BST.
+     *
+     * @param      x     TheNode to be deleted.
+     *
+     * @return     Deleted Node.
+     */
+    private Node deleteMax(final Node x) {
+        if (x.right == null) {
+            return x.left;
+        }
+        x.right = deleteMax(x.right);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    /**
+     * Time complexity is O(N).
+     * Removes the specified key and
+     * its associated value from this symbol table
+     * (if the key is in this symbol table).
+     *
+     * @param  key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(final Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException(
+                "calls delete() with a null key");
+        }
+        root = delete(root, key);
+        //assert check();
+    }
+    /**
+     * Time complexity is O(N).
+     * delete the Node whose key is given.
+     *
+     * @param      x     The Node.
+     * @param      key   The key
+     *
+     * @return     The Node.
+     */
+    private Node delete(final Node x, final Key key) {
+        Node temp = x;
+        if (temp == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(temp.key);
+        if      (cmp < 0) {
+            temp.left  = delete(temp.left,  key);
+        } else if (cmp > 0) {
+            temp.right = delete(temp.right, key);
+        } else {
+            if (temp.right == null) {
+                return temp.left;
+            }
+            if (temp.left  == null) {
+                return temp.right;
+            }
+            Node t = temp;
+            temp = min(t.right);
+            temp.right = deleteMin(t.right);
+            temp.left = t.left;
+        }
+        temp.size = size(temp.left) + size(temp.right) + 1;
+        return temp;
+    }
+
+
+
+    /**
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     *
+     * @return all keys in the symbol table
+     */
+    public Iterable<Key> keys() {
+        if (isEmpty()) return new Queue<Key>();
+        return keys(min(), max());
+    }
+
+    /**
+     * Returns all keys in the symbol table in the given range,
+     * as an {@code Iterable}.
+     *
+     * @param  lo minimum endpoint
+     * @param  hi maximum endpoint
+     * @return all keys in the symbol table between {@code lo} 
+     *         (inclusive) and {@code hi} (inclusive)
+     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
+     *         is {@code null}
+     */
+    public Iterable<Key> keys(Key lo, Key hi) {
+        if (isEmpty()) return new Queue<Key>();
+        if (lo == null) throw new IllegalArgumentException("first argument to keys() is null");
+        if (hi == null) throw new IllegalArgumentException("second argument to keys() is null");
+
+        Queue<Key> queue = new Queue<Key>();
+        keys(root, queue, lo, hi);
+        return queue;
+    } 
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) { 
+        if (x == null) return; 
+        int cmplo = lo.compareTo(x.key); 
+        int cmphi = hi.compareTo(x.key); 
+        if (cmplo < 0) keys(x.left, queue, lo, hi); 
+        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key); 
+        if (cmphi > 0) keys(x.right, queue, lo, hi); 
+    }
+}
---
+++ ADS-1-practice/m24/2/Solution.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+/**
+ * Solution class.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * Main methods handles the input and output methods.
+     *
+     * @param      args  The arguments at command line.
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = Integer.parseInt(scan.nextLine());
+        BST<Student, String> bs = new BST<Student, String>();
+        for (int i = 0; i < num; i++) {
+            String[] input = scan.nextLine().split(",");
+            Student s = new Student(
+                input[0], input[1], Double.parseDouble(input[2]));
+            bs.put(s, input[0]);
+        }
+        int num1 = Integer.parseInt(scan.nextLine());
+        for (int j = 0; j < num1; j++) {
+            String[] command = scan.nextLine().split(" ");
+            if (command[0].equals("BE")) {
+                for(Student s1: bs.keys()) {
+                    if(s1.getMarks() >= Double.parseDouble(command[1]) &&
+                        s1.getMarks() <= Double.parseDouble(command[2])) {
+                        System.out.println(s1.getName());
+                    }
+                }
+
+            } 
+            else if (command[0].equals("GE")) {
+                for(Student s1: bs.keys()) {
+                    if(s1.getMarks() <= Double.parseDouble(command[1])) {
+                        System.out.println(s1.getName());
+                    }
+                }
+            } else if (command[0].equals("LE")) {
+               for(Student s1: bs.keys()) {
+                    if(s1.getMarks() >= Double.parseDouble(command[1])) {
+                        System.out.println(s1.getName());
+                    }
+                } 
+            }
+        }
+    }
+}
